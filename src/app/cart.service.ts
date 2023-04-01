e9a67f78f8a110c31e7fec0108b9e72569688496
youpong <43817652+youpong@users.noreply.github.com>
2023-04-02 01:44:49 +0900

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -22,4 +22,8 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
 }
