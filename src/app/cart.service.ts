e78a19b6e61422bdd4cc8b921a471c786b979d8e
youpong <43817652+youpong@users.noreply.github.com>
2023-04-02 01:42:53 +0900

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 
@@ -6,7 +7,8 @@ import { Injectable } from '@angular/core';
 })
 export class CartService {
   items: Product[] = [];
-  constructor() {}
+
+  constructor(private http: HttpClient) {}
 
   addToCart(product: Product) {
     this.items.push(product);
