450ddbd84b613c7eccae384dda022c2a324a774b
youpong <43817652+youpong@users.noreply.github.com>
2023-04-02 01:37:17 +0900

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -4,8 +4,10 @@ import { CartService } from '../cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  items = this.cartService.getItems();
 
+  constructor(private cartService: CartService) {}
 }
