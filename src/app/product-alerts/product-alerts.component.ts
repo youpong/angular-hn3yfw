1bccdb0955195765511bc27db3964dd563ea89cf
NAKAJIMA Yusaku <yupong7@gmail.com>
2023-06-06 01:13:46 +0900

    remove css files

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -4,13 +4,14 @@ import { Product } from '../products';
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent implements OnInit {
   constructor() {}
   ngOnInit(): void {}
 
   @Input() product!: Product;
-  
+
+  // @Output() allows the ProductAlertsComponent to emit an event when
+  // the value of the notify property changes.
   @Output() notify = new EventEmitter();
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,7 +5,6 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = products;
@@ -18,9 +17,3 @@ export class ProductListComponent {
     window.alert('You will be notified when the product goes on sale');
   }
 }
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
